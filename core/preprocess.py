05ff1e69c08ac96b1eda01e109f3e8d4ef229d09
Mohammad Taiba <mohammadtaiba54@gmail.com>
2025-11-17 09:34:07 +0100

    refactor: preprocess.py

--- core/preprocess.py
+++ core/preprocess.py
@@ -3,27 +3,27 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-# Projektroot --> `.env`-datei laden
+# Projekt root --> `.env`-datei laden
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 load_dotenv(PROJECT_ROOT / ".env")
 
-# --- Modelle / API ---
+# Modelle / API
 OLLAMA_URL  = os.getenv("OLLAMA_URL")
 EMBED_MODEL = os.getenv("EMBED_MODEL")
 LLM_MODEL   = os.getenv("LLM_MODEL")
 COLLECTION  = os.getenv("COLLECTION")
 
-# --- Datenpfade (zentrales Single-Source-of-Truth) ---
+# Datenpfade (zentrales Single-Source-of-Truth)
 RAW_PATH      = Path(os.getenv("DATA_RAW"))
 UPLOAD_PATH   = Path(os.getenv("DATA_UPLOAD"))
 TEMP_MD_PATH  = Path(os.getenv("DATA_TEMP_MD"))
 FINAL_MD_PATH = Path(os.getenv("DATA_MARKDOWN"))
 DB_PATH       = Path(os.getenv("DB_PATH"))
 
-# Verzeichnisse EINMALIG sicherstellen
-for p in (RAW_PATH, UPLOAD_PATH, TEMP_MD_PATH, FINAL_MD_PATH, DB_PATH):
-    p.mkdir(parents=True, exist_ok=True)
+# Verzeichnisse sicherstellen
+for folder in (RAW_PATH, UPLOAD_PATH, TEMP_MD_PATH, FINAL_MD_PATH, DB_PATH):
+    folder.mkdir(parents=True, exist_ok=True)
 
-# --- Chunking ---
+# Chunking
 CHUNK_SIZE    = os.getenv("CHUNK_SIZE")
 CHUNK_OVERLAP = os.getenv("CHUNK_OVERLAP")
