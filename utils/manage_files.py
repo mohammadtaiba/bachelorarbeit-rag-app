fa2f70846b1ac1dcb7e3157f939191857178a45f
Mohammad Taiba <mohammadtaiba54@gmail.com>
2025-12-10 14:26:57 +0100

    feat: Konvertierung mit (txt, md) erweitert + refactoring

--- utils/manage_files.py
+++ utils/manage_files.py
@@ -10,7 +10,7 @@ from core.preprocess import PATH_PROCESSING, PATH_PROCESSED, PATH_UPLOAD, PATH_R
 # Konvertiere alle Dateien in der UPLOAD zu MD-Dateien
 # ======================================================================================================
 def convert_all_to_markdown():
-    logger.info("Starte Konvertierung der Rohdaten zu Markdown ...")
+    logger.info(f"Starte Konvertierung der Rohdaten aus dem Ordner ({PATH_UPLOAD.resolve()}) zu Markdown ...")
 
     # -----------------------------------------------------------------------------------------------------------------
     # pdf2md
@@ -19,7 +19,7 @@ def convert_all_to_markdown():
     pdf_files    = sorted(PATH_UPLOAD.glob("*.pdf"))
 
     if not pdf_files:
-        logger.info(f"Keine PDFs in {PATH_UPLOAD.resolve()} gefunden.")
+        logger.info(f"    - Es existiert keine PDFs.")
     else:
         for pdf in pdf_files:
             try:
@@ -27,19 +27,19 @@ def convert_all_to_markdown():
                 md_text = result.text_content
 
                 (PATH_PROCESSING / (pdf.stem + ".md")).write_text(md_text, encoding="utf-8")
-                logger.info(f"    - konvertiert: {pdf.name}")
+                logger.debug(f"    - konvertiert: {pdf.name}")
 
             except Exception as e:
                 logger.exception(f"⚠️ Fehler bei der Konvertierung der PDF-Datei {pdf.name}")
 
-        logger.info(f"Anzahl der konvertierten PDFs: {len(pdf_files)}.")
+        logger.info(f"    - Anzahl der konvertierten PDFs: {len(pdf_files)}.")
 
     # -----------------------------------------------------------------------------------------------------------------
     # docx2gfm
     # -----------------------------------------------------------------------------------------------------------------
     docx_files = sorted(PATH_UPLOAD.glob("*.docx"))
     if not docx_files:
-        logger.info(f"Keine DOCX in {PATH_UPLOAD.resolve()} gefunden.")
+        logger.info(f"    - Es existiert keine DOCX.")
     else:
         md_converter = MarkItDown()
 
@@ -49,19 +49,19 @@ def convert_all_to_markdown():
                 md_text = result.text_content
 
                 (PATH_PROCESSING / (docx.stem + ".md")).write_text(md_text, encoding="utf-8")
-                logger.info(f"    - konvertiert: {docx.name}")
+                logger.debug(f"    - konvertiert: {docx.name}")
 
             except Exception as e:
                 logger.exception(f"⚠️ Fehler bei der Konvertierung der Word-Datei {docx.name}")
 
-        logger.info(f"Anzahl der konvertierten DOCX: {len(docx_files)}.")
+        logger.info(f"    - Anzahl der konvertierten DOCX: {len(docx_files)}.")
 
     # -----------------------------------------------------------------------------------------------------------------
     # xlsx2md (optimiert)
     # -----------------------------------------------------------------------------------------------------------------
     xlsx_files = sorted(PATH_UPLOAD.glob("*.xlsx")) + sorted(PATH_UPLOAD.glob("*.xls"))
     if not xlsx_files:
-        logger.info(f"Keine Excel-Dateien in {PATH_UPLOAD.resolve()} gefunden.")
+        logger.info(f"    - Es existiert keine XLSXs.")
     else:
         import warnings
         warnings.filterwarnings("ignore", category=UserWarning, module="openpyxl")
@@ -87,12 +87,39 @@ def convert_all_to_markdown():
                     continue
 
                 (PATH_PROCESSING / f"{xlsx.stem}.md").write_text("\n\n".join(md_parts), encoding="utf-8")
-                logger.info(f"    - konvertiert: {xlsx.name}")
+                logger.debug(f"    - konvertiert: {xlsx.name}")
 
             except Exception as e:
                 logger.exception(f"⚠️ Fehler bei der Konvertierung der Excel-Datei {xlsx.name}")
 
-        logger.info(f"Anzahl der konvertierten Excel-Dateien: {len(xlsx_files)}.")
+        logger.info(f"    - Anzahl der konvertierten Excel-Dateien: {len(xlsx_files)}.")
+
+
+    # -----------------------------------------------------------------------------------------------------------------
+    # txt-dateien umbenennen und nach processing verschieben
+    # -----------------------------------------------------------------------------------------------------------------
+    txt_files = sorted(PATH_UPLOAD.glob("*.txt"))
+    if not txt_files:
+        logger.info(f"    - Es existiert keine TXTs.")
+    else:
+        for txt in txt_files:
+            target = PATH_PROCESSING / f"{txt.stem}.md" # umbenennen
+            txt.rename(target) # verschieben
+            logger.debug(f"    - konvertiert: {txt.name}")
+        logger.info(f"    - Anzahl der konvertierten TXT-Dateien: {len(txt_files)}.")
+
+    # -----------------------------------------------------------------------------------------------------------------
+    # markdown-dateien automatisch nach processing verschieben
+    # -----------------------------------------------------------------------------------------------------------------
+    md_files = sorted(PATH_UPLOAD.glob("*.md"))
+    if not md_files:
+        logger.info(f"    - Es existiert keine MDs.")
+    else:
+        for md in md_files:
+            target = PATH_PROCESSING / md.name
+            md.rename(target) # Datei verschieben
+            logger.debug(f"    - konvertiert: {md.name}")
+        logger.info(f"    - Anzahl der konvertierten MD-Dateien: {len(md_files)}.")
 
     # -----------------------------------------------------------------------------------------------------------------
     logger.info("Konvertierung abgeschlossen.")
@@ -113,7 +140,7 @@ def move_processing2processed():
         try:
             target = PATH_PROCESSED / file.name
             file.replace(target)  # schneller als shutil.move für gleiche Partition
-            logger.info(f"    - {file.name} → nach {PATH_PROCESSED.resolve()} verschoben")
+            logger.debug(f"    - {file.name} → nach {PATH_PROCESSED.resolve()} verschoben")
         except Exception as e:
             logger.exception("⚠️ Fehler beim Verschieben vom TEMP_MD_Ordner zu FINAL_MD_Ordner.")
     logger.info(f"Verschiebung abgeschlossen.")
