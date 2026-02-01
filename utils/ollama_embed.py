5311cfe837eb3da040f1d779151dfbc06b281b91
mohammad taiba <mohammadtaiba54@gmail.com>
2026-02-01 16:42:39 +0100

    chore: ollama_embed.py (logging)

--- utils/ollama_embed.py
+++ utils/ollama_embed.py
@@ -28,4 +28,4 @@ class OllamaEmbeddings(Embeddings):
         response.raise_for_status()
         return response.json()["embedding"]
 
-    logger.info("Das RAG-System ist jetzt mit LMStudio verbunden.")
+    logger.info("Das RAG-System ist jetzt mit Ollama-Endpunkt verbunden.")
